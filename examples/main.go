17502fdfd1550c369d19cd27163acc6cb05c3fc4
Dongri Jin <dongrify@gmail.com>
2016-04-19 00:50:28 +0900

    Fix examples

--- examples/main.go
+++ examples/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 
-	line "github.com/dongri/line-bot-client"
+	"github.com/dongri/LineBot"
 )
 
-var botClient *line.Client
+var botClient *LineBot.Client
 
 func main() {
 	channelID := os.Getenv("LINE_CHANNEL_ID")
@@ -18,7 +20,11 @@ func main() {
 	mid := os.Getenv("LINE_MID")
 	proxyURL := getProxyURL() // can set nil if not need
 
-	botClient = line.NewClient(line.EndPoint, channelID, channelSecret, mid, proxyURL)
+	botClient = LineBot.NewClient(LineBot.EndPoint, channelID, channelSecret, mid, proxyURL)
+
+	// EventHandler
+	var myEvent LineBot.EventHandler = NewEventHandler()
+	botClient.SetEventHandler(myEvent)
 
 	http.HandleFunc("/callback", callbackHandler)
 	port := os.Getenv("PORT")
@@ -27,133 +33,7 @@ func main() {
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	m, err := botClient.ReceiveMessageAndOperation(r.Body)
-	if err != nil {
-		log.Print(err)
-	}
-	for _, result := range m.Result {
-		from := result.Content.From
-
-		if result.Content.OpType == line.OpTypeAdded {
-			from = result.Content.Params[0]
-			botClient.SendText([]string{from}, "友達追加してくれてありがとうね！")
-			log.Print("Added as friend")
-			return
-		}
-
-		if result.Content.OpType == line.OpTypeBlocked {
-			from = result.Content.Params[0]
-			botClient.SendText([]string{from}, "あらら、いつでもブロック解除できますよー")
-			log.Print("Blocked account")
-			return
-		}
-
-		// Get User Profile
-		fromUser, err := botClient.GetUserProfiles(from)
-		if err != nil {
-			log.Print(err)
-		}
-		displayName := fromUser.Contacts[0].DisplayName
-
-		// Send Text
-		text := result.Content.Text
-		sentResult, err := botClient.SendText([]string{from}, text+"\n\nBy "+displayName)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Sticker
-		STKID := "2"
-		STKPKGID := "1"
-		STKVER := "100"
-		STKTXT := "Text"
-		sentResult, err = botClient.SendSticker([]string{from}, STKID, STKPKGID, STKVER, STKTXT)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Image
-		imageOriginalContentURL := "https://example.com/test_original.png"
-		imagePreviewImageURL := "https://example.com/test_preview.png"
-		sentResult, err = botClient.SendImage([]string{from}, imageOriginalContentURL, imagePreviewImageURL)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Video ....
-		videoOriginalContentURL := "https://example.com/test.mp4"
-		videoPreviewImageURL := "http://example.com/test.png"
-		sentResult, err = botClient.SendVideo([]string{from}, videoOriginalContentURL, videoPreviewImageURL)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Audio ....
-		audioOriginalContentURL := "https://example.com/test.mp3"
-		audlen := "240000"
-		sentResult, err = botClient.SendAudio([]string{from}, audioOriginalContentURL, audlen)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Contact ....
-		sentResult, err = botClient.SendContact([]string{from}, from, displayName)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send Rich Message
-		downloadURL := "https://farm1.staticflickr.com/715/22658699705_7591e8d0a6_b.jpg"
-		altText := "リスト画面に表示される文字列"
-		markupJSON := getMarkupJSON()
-		sentResult, err = botClient.SendRichMessage([]string{from}, downloadURL, altText, markupJSON)
-		if err != nil {
-			log.Print(err)
-		}
-		if len(sentResult.Failed) == 0 {
-			log.Print("Failed")
-			return
-		}
-
-		// Send MultipleMessage
-		messageNotified := 0
-		var contents []line.Content
-		textContent := new(line.Content)
-		textContent.Text = "hoge"
-		contents = append(contents, *textContent)
-
-		imageContent := new(line.Content)
-		imageContent.OriginalContentURL = "https://farm1.staticflickr.com/715/22658699705_7591e8d0a6_b.jpg"
-		contents = append(contents, *imageContent)
-		botClient.SendMultipleMessage([]string{from}, messageNotified, contents)
-
-	}
-
-	log.Print("Success")
+	log.Print("=== callback ===")
 }
 
 func getProxyURL() *url.URL {
@@ -164,6 +44,66 @@ func getProxyURL() *url.URL {
 	return proxyURL
 }
 
-func getMarkupJSON() string {
-	return "{\"canvas\": {\"width\": 1024,\"height\": 576,\"initialScene\": \"scene1\"},\"images\": {\"image1\": {\"x\": 0,\"y\": 0,\"w\": 1024,\"h\": 576}},\"actions\": {\"openHomepage\": {\"type\": \"web\",\"text\": \"Open link1.\",\"params\": {\"linkUri\": \"http://dongri.github.io/\"}},\"showItem\": {\"type\": \"web\",\"text\": \"Open link2.\",\"params\": {\"linkUri\": \"https://dongri.github.io/post/2016-02-22-the-programmer-hierarchy/\"}}},\"scenes\": {\"scene1\": {\"draws\": [{\"image\": \"image1\",\"x\": 0,\"y\": 0,\"w\": 1024,\"h\": 576}],\"listeners\": [{\"type\": \"touch\",\"params\": [0, 0, 1024, 250],\"action\": \"openHomepage\"}, {\"type\": \"touch\",\"params\": [0, 250, 1024, 326],\"action\": \"showItem\"}]}}}"
+// BotEventHandler ...
+type BotEventHandler struct {
+}
+
+// NewEventHandler ...
+func NewEventHandler() *BotEventHandler {
+	return &BotEventHandler{}
+}
+
+// OnAddedAsFriendOperation ...
+func (be *BotEventHandler) OnAddedAsFriendOperation(mids []string) {
+	botClient.SendText(mids, "友達追加してくれてありがとうね！")
+}
+
+// OnBlockedAccountOperation ...
+func (be *BotEventHandler) OnBlockedAccountOperation(mids []string) {
+	botClient.SendText(mids, "あらら,,, (このメッセージは届かない)")
+}
+
+// OnTextMessage ...
+func (be *BotEventHandler) OnTextMessage(from, text string) {
+	botClient.SendText([]string{from}, text)
+}
+
+// OnImageMessage ...
+func (be *BotEventHandler) OnImageMessage(from string) {
+	log.Print("=== Received Image ===")
+	// SendImage
+	originalContentURL := "http://weknowyourdreamz.com/image.php?pic=/images/robot/robot-03.jpg"
+	previewImageURL := "http://weknowyourdreamz.com/image.php?pic=/images/robot/robot-03.jpg"
+	botClient.SendImage([]string{from}, originalContentURL, previewImageURL)
+}
+
+// OnVideoMessage ...
+func (be *BotEventHandler) OnVideoMessage(from string) {
+	log.Print("=== Received Video ===")
+}
+
+// OnAudioMessage ...
+func (be *BotEventHandler) OnAudioMessage(from string) {
+	log.Print("=== Received Audio ===")
+}
+
+// OnLocationMessage ...
+func (be *BotEventHandler) OnLocationMessage(from, title, address string, latitude, longitude float64) {
+	log.Print("=== Received Location ===")
+}
+
+// OnStickerMessage ...
+func (be *BotEventHandler) OnStickerMessage(from, stickerPackageID, stickerID, stickerVersion, stickerText string) {
+	log.Print("=== Received Sticker ===")
+	// Send Random Sticker
+	r := rand.Intn(10) + 1
+	stkID := strconv.Itoa(r)
+	stkpkgID := "1"
+	stkVer := "100"
+	botClient.SendSticker([]string{from}, stkID, stkpkgID, stkVer, "hoge")
+}
+
+// OnContactMessage ...
+func (be *BotEventHandler) OnContactMessage(from, MID, displayName string) {
+	log.Print("=== Received Contact ===")
 }
